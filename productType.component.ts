2f23c5ff99068b51a6bcc8ed0a215f56c9ab0b5a
nikhilmundhra <37223509+nikhilmundhra@users.noreply.github.com>
2018-07-10 13:17:02 +0530

    Add files via upload

---
+++ productType.component.ts
@@ -0,0 +1,24 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { IProductType } from './productType';
+import { ProductService } from '../product/product.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'product-type',
+    templateUrl: './productType.component.html',
+    styleUrls: ['./productType.component.css']
+})
+
+export class ProductTypeComponent implements OnInit {
+    productType: IProductType;
+
+    constructor(private _productService: ProductService, private _activatedRoute: ActivatedRoute) {
+
+    }
+
+    ngOnInit() {
+        let TypeId: number = this._activatedRoute.snapshot.params['id'];
+        this._productService.getProductTypeByID(TypeId)
+            .subscribe((productTypeData) => this.productType = productTypeData);
+    }
+}
---
+++ productType.ts
@@ -0,0 +1,5 @@
+﻿export interface IProductType {
+    id: number;
+    Name: string;
+    Description: string;
+}
